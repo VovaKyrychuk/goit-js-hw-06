69a6a99a6e8a029a525e0db4becb31dddd72204b
VovaKyrychuk <kyrychuk81@gmail.com>
2023-02-11 18:59:52 +0200

    task2 - 3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+//console.log(ingredients);
+
+const ingredEl = ingredients.map((item) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.classList.add("item");
+  return ingredient;
+});
+//console.log(ingredEl);
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...ingredEl);
