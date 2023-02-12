09857e87ebcb43fa07748df7050130c2203f05f3
VovaKyrychuk <kyrychuk81@gmail.com>
2023-02-12 19:23:55 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const valueColor = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+//console.log(valueColor);
+//console.log(changeColorButton);
+
+const bodyBg = document.body;
+
+const changeColor = () => {
+  valueColor.textContent = getRandomHexColor();
+  bodyBg.style.backgroundColor = valueColor.textContent;
+};
+changeColorButton.addEventListener("click", changeColor);
